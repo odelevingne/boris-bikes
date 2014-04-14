4e6f5afa29b84c8fea51bdc938a4b8e7cb160309
Oliver Delevingne <oliver@delevingne.co.uk>
2014-04-14 12:45:43 +0100

    added new test so that bikes should be able to break, introduced new method 'break', and the new instanace variable 'broken'

--- spec/bikes_spec.rb
+++ spec/bikes_spec.rb
@@ -6,4 +6,10 @@ describe "Bikes" do
 		bikes = Bikes.new
 		expect(bikes).not_to be_broken
 	end
+
+	it "should be able to break" do
+		bikes = Bikes.new
+		bikes.break
+		expect(bikes).to be_broken
+	end
 end
