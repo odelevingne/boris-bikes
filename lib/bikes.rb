3f722541947287fe521be14909f87f3778a25c3c
Oliver Delevingne <oliver@delevingne.co.uk>
2014-04-15 13:47:27 +0100

    adding docking station functionality, including all pasing tests

--- lib/bikes.rb
+++ lib/bikes.rb
@@ -1,4 +1,4 @@
-class Bikes
+class Bike
 
 def initialize 
 	fix!
--- spec/bikes_spec.rb
+++ spec/bikes_spec.rb
@@ -1,18 +1,24 @@
 require "bikes"
 
-describe "Bikes" do 
-	let(:bikes) {Bikes.new}
+describe "Bike" do 
+	let(:bike) {Bike.new}
 	it "should not be broken" do 
-		expect(bikes).not_to be_broken
+		expect(bike).not_to be_broken
 	end
 
 	it "should be able to break" do
-		bikes.break!
-		expect(bikes).to be_broken
+		bike.break!
+		expect(bike).to be_broken
 	end
 
 	it "should be able to fix if broken" do
-		bikes.break!.fix!
-		expect(bikes).not_to be_broken
+		bike.break!.fix!
+		expect(bike).not_to be_broken
 	end
 end
+
+
+
+
+
+
